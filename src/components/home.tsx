d73cb11b1aff6fdadfeb9dc44dcb624038e7a952
Cody Grogan <cod.grogan@gmail.com>
2022-02-28 22:09:22 +0800

    section styles

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -9,14 +9,22 @@ function Home(props: any){
         
         <div>
 
+        <section className='portfolioSection color1'>
+
         <div data-aos="fade-up">
             Hello World
         </div>
 
+        </section>
+
+
+        <section className='portfolioSection color2'>
+
+
         <div data-aos="fade-down">
             Hello World again
         </div>
-
+        </section>
 
 </div>
 
