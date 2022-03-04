96647528bac0459ed535d2643d2c26e36d1d4064
Cody Grogan <cod.grogan@gmail.com>
2022-03-04 18:37:09 +0800

    reformated page, added language dropdown

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,6 @@
 import Aos from 'aos';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Intro from './Intro';
-import IntroParticle from './particle';
 
 
 function Home(props: any){
@@ -12,170 +10,84 @@ function Home(props: any){
 
     return(
         
-        <div className='homeContainer'>
-        <section className='portfolioSectionStart color1'>
-            
+        <div className='home'>
 
 
-            <div data-aos="fade-up fade-out">
-                <IntroParticle/>
-            </div>
 
-            <div>
-                <Intro/>
-            </div>
-
-
-        </section>
-
-
-        <section className='portfolioSection color2'>
         
-            <div className='sectionHead '>
-            <h2>Skills</h2>
+          <div className="container-fluid bg-light  bg-gradient">
+            <div className='row'>
+
+            <div className='col photoDiv'>
+              photo here
             </div>
 
-        <div className='skillContainer'>
+            <div className='col'>
+            <h1 >Cody Grogan</h1>
+            <p >Full Stack Web Developer</p>
 
-        <div className='lineBreak'/>
-            <div className='skillGroup ' data-aos="fade-left">
-            <h3>Front End</h3>   
-                <div className='skillInnerGroup'>
+            <p>
+
+
+            </p>
+
+            </div>
+          
+            
+            <div className='col contactBtn'>
+              <div>
+          
+            <button className="btn btn-outline-success" type="submit">Contact Me</button>
+            </div>
+
+            </div>
+            <div className='col'>
+            <div >
+                <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                <span className="material-icons">
+                  language
+                  </span>
+                </a>
+
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <li><a className="dropdown-item" href="#">English</a></li>
+                  <li><a className="dropdown-item" href="#">中文</a></li>
+                </ul>
+              </div>
+
+              </div>
+              </div>
+          </div>
+     
+          <div className='homeElement'>
+
+          Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsuLorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est m dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est 
+          </div>
                 
-                       
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/logo192.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/html5.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/css.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/javascript.png'></img>
-                </div>
-
-                </div>
-
-             
-            </div>
-
-
-            <div className='skillGroup' data-aos="fade-right">
-            <h3>Back End</h3>  
-
-                <div className='skillInnerGroup'>
-
-                                 
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/node.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/Expressjs.png'></img>
-                </div>
-
-                </div>
-
-            </div>
-            <div className='skillGroup' data-aos="fade-left">
-            
-            <h3>Misc</h3>    
-                <div className='skillInnerGroup'>
-              
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/mongo.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/github.png'></img>
-                </div>
+        <div className='homeElement'>
+          <h2>
+            Portfolio
+          </h2>
 
 
 
-                </div>
+        </div>
 
-            </div>
-            </div>
+      
 
-        </section>
+        <div className='homeElement'>
 
-
-        <section className='portfolioSection color1'>
-        <h2>Portofolio</h2>
-
-        <div className='lineBreak'/>
+          Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsuLorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est m dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est 
+        </div>
 
 
-            <div data-aos="fade-up">
+        <div className='homeElement'>
 
-                Fancy introduction to my portfolio projects
-            </div>
-
-            <div className='skillGroup' data-aos="fade-left">
-            
-            <h3>Misc</h3>    
-                <div className='skillInnerGroup'>
-              
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/mongo.png'></img>
-                </div>
-
-                <div className='skillContainer'>
-                <img className='aboutIcon' src='./img/github.png'></img>
-                </div>
+          Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsuLorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est m dolorem est Lorem ipsum dolorem est Lorem ipsum dolorem est 
+        </div>
 
 
-
-                </div>
-
-            </div>
-
-            <div className='skillDrawer'>
-            <div className='skillBubble'>
-            <img className='aboutIcon' src='./img/github.png'></img>
-            </div>
-
-            Github
-
-
-
-
-            </div>
-
-            <div className='skillDrawer'>
-                github
-            </div>
-
-        </section>
-
-
-    <section className='portfolioSection color2'>
-        <h2>About Me</h2>
-
-    <div data-aos="fade-down">
-        Education, nontraditional background, Chinese skills
-    </div>
-    </section>
-
-    <section className='portfolioSection color1'>
-        <h2>Contact</h2>
-        <div className='lineBreak'/>
-
-
-            <div data-aos="fade-up">
-
-                My linked in, email me, yadada
-            </div>
-
-    </section>
-
-
-</div>
+        </div>
 
 
 
