73fd1b0a9c63a18a9458e15cf0eabe8185ead037
Cody Grogan <cod.grogan@gmail.com>
2022-03-08 19:29:02 +0800

    madlib carousel

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -268,7 +268,36 @@ function Home(props: any){
         </div>
 
         <div className='col-sm'>
-        <img src='./img/image_128.png'/>
+        <div id="carouselMadlib" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselMadlib" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselMadlib" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselMadlib" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                          <button type="button" data-bs-target="#carouselMadlib" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        </div>
+                        <div className="carousel-inner">
+                          <div className="carousel-item active">
+                            <img src={"/img/mad/mad1-f.PNG"} className="d-block w-100 carImg" alt="dgt1"/>
+                          </div>
+                          <div className="carousel-item">
+                            <img src={"/img/mad/mad2-f.PNG"} className="d-block w-100 carImg" alt="..."/>
+                          </div>
+                          <div className="carousel-item">
+                            <img src={"/img/mad/mad3-f.PNG"} className="d-block w-100 carImg" alt="..."/>
+                          </div>
+                          <div className="carousel-item">
+                            <img src={"/img/mad/mad4-f.PNG"} className="d-block w-100 carImg" alt="..."/>
+                          </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselMadlib" data-bs-slide="prev">
+                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselMadlib" data-bs-slide="next">
+                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span className="visually-hidden">Next</span>
+                        </button>
+                      </div>
         </div> 
       </div>
     </div>
