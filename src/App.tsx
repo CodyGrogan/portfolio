ebe3e28302008e62cb257f12f81e2c21624bd5af
Cody Grogan <cod.grogan@gmail.com>
2022-03-18 16:14:56 +0800

    fixed bug on deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
 import Chinese from './components/chinese';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-   
-
-      <BrowserRouter>
+        <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/zh' element={<Chinese/>}></Route>
@@ -20,6 +18,7 @@ function App() {
       </Routes>
       
       </BrowserRouter>
+      <Outlet />
     </div>
   );
 }
